e5af26f262645ca731c63d749babf5a41e86705f
kylem <kylemcnamara2000@gmail.com>
2018-12-07 20:23:00 -0600

    safety save

--- app/src/main/java/com/fall2018finalproject/cs125/enhancify/PlaylistGeneratorSongs.java
+++ app/src/main/java/com/fall2018finalproject/cs125/enhancify/PlaylistGeneratorSongs.java
@@ -1,13 +1,12 @@
 package com.fall2018finalproject.cs125.enhancify;
 
-
-
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -16,100 +15,55 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
+public class PlaylistGeneratorSongs extends AppCompatActivity {
 
-
-/**
- * Main screen for our API testing app.
- */
-public final class MainActivity extends AppCompatActivity {
     /** Default logging tag for messages from the main activity. */
-    private static final String TAG = "Main";
+    private static final String TAG = "PlaylistGenerator";
+
+    /** Base 64 encoded client_id:client_client_secret. */
+    private static final String COMBINED_KEY = "ZTYwZWFlZTk0MWMyNGQyMTkyMGFlMWZiZjM5MDQ2NWI6OTlhY2U0YTdhYzkyNDY2NGE2NWFkOWNmNDBkNjllYWE=";
 
     /** Request queue for our network requests. */
     private static RequestQueue requestQueue;
 
-    /** Base 64 encoded client_id:client_client_secret. */
-    private static final String COMBINED_KEY = "ZTYwZWFlZTk0MWMyNGQyMTkyMGFlMWZiZjM5MDQ2NWI6OTlhY2U0YTdhYzkyNDY2NGE2NWFkOWNmNDBkNjllYWE=";
-    /**
-     * Run when our activity comes into view.
-     *
-     * @param savedInstanceState state that was saved by the activity last time it was paused
-     */
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Set up a queue for our Volley requests
         requestQueue = Volley.newRequestQueue(this);
 
-        // Load the main layout for our activity
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_playlist_generator_songs);
 
-        // Attach the handler to our UI button
-        final Button tokenAPICall = findViewById(R.id.startAPICall);
-        tokenAPICall.setOnClickListener(new View.OnClickListener() {
+        configureBackButton();
+
+        final Button generatePlaylist = findViewById(R.id.generatePlaylist);
+        generatePlaylist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                Log.d(TAG, "Start API button clicked");
+                Log.d(TAG, "generatePlaylist button hit.");
 
                 //dummy();
                 tokenAPICall();
             }
         });
-
-        // Make sure that our progress bar isn't spinning and style it a bit
-        //ProgressBar progressBar = findViewById(R.id.progressBar);
-        //progressBar.setVisibility(View.INVISIBLE);
-
-        configureNextButton();
-        configureKyleButton();
     }
 
-    private void configureNextButton() {
-        Button nextButton = (Button) findViewById(R.id.nextButton);
-        nextButton.setOnClickListener(new View.OnClickListener() {
+    private void configureBackButton(){
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, PlaylistGenerator.class));
+                finish();
             }
         });
     }
-
-
-    //set up button that sends the user to song to playlist generator
-    private void configureKyleButton() {
-        Button nextButton = (Button) findViewById(R.id.kyleButton);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, PlaylistGeneratorSongs.class));
-            }
-        });
-    }
-
-
-    /**
-     * Make an API call.
-     */
-
-
-    void dummy() {
-        APIRequest apiRequest = new APIRequest(this);
-        requestQueue.start();
-        JSONObject temp = apiRequest.callApiGET(
-                "blah", "https://api.spotify.com/v1/browse/new-releases");
-        TextView output = findViewById(R.id.jsonResult);
-        output.setText(temp.toString());
-    }
-
-
-
 
     void tokenAPICall() {
 
@@ -127,12 +81,11 @@ public final class MainActivity extends AppCompatActivity {
                             TextView output = findViewById(R.id.jsonResult);
                             try {
                                 String accessKey = response.getString("access_token");
-                                Log.d(TAG, "access_token: " + accessKey);
                                 spotifyAPICall(accessKey);
                             } catch (JSONException e) {
                                 Log.d(TAG, "oops");
                             }
-                            output.setText(response.toString());
+                            //output.setText(response.toString());
                         }
                     }, new Response.ErrorListener() {
                 @Override
@@ -163,17 +116,25 @@ public final class MainActivity extends AppCompatActivity {
      * @param authorizationKey Key in order to use spotify's servers.
      */
     void spotifyAPICall(final String authorizationKey) {
+        Log.d(TAG, queryBuilder());
         try {
             CustomRequest jsonObjectRequest = new CustomRequest(
                     Request.Method.GET,
-                    "https://api.spotify.com/v1/browse/new-releases",
+                    "https://api.spotify.com/v1/recommendations" + queryBuilder(),
                     null,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(final JSONObject response) {
                             Log.d(TAG, response.toString());
-                            TextView output = findViewById(R.id.jsonResult);
-                            output.setText(response.toString());
+                            try {
+                                JSONArray trackArray = response.getJSONArray("tracks");
+                                for (int trackIndex = 0; trackIndex < trackArray.length(); trackIndex++) {
+                                    //Log.d("");
+                                }
+                                //Log.d(TAG, "tracks: " + tracks);
+                            } catch (JSONException e) {
+                                Log.d(TAG, "something failed");
+                            }
                         }
                     }, new Response.ErrorListener() {
                 @Override
@@ -197,5 +158,18 @@ public final class MainActivity extends AppCompatActivity {
             Log.d(TAG, "Authorization failed");
             e.printStackTrace();
         }
+    }
+
+    private String queryBuilder() {
+        String complete = "?limit=10&market=US&seed_genres=";
+        EditText genreInput = findViewById(R.id.genreInput);
+        complete = complete + genreInput.getText().toString() + "&";
+        complete = complete + "target_tempo" + Integer.toString((((SeekBar) findViewById(R.id.tempoBar)).getProgress()) + 30) + "&";
+        complete = complete + "target_valence" + Double.toString((double) (((SeekBar) findViewById(R.id.valanceBar)).getProgress()) / 100.0) + "&";
+        complete = complete + "target_popularity" + Integer.toString(((SeekBar) findViewById(R.id.popularityBar)).getProgress()) + "&";
+        complete = complete + "target_danceability" + Double.toString((double) (((SeekBar) findViewById(R.id.danceabilityBar)).getProgress()) / 100.0) + "&";
+        complete = complete + "target_energy" + Double.toString((double) (((SeekBar) findViewById(R.id.energyBar)).getProgress()) / 100.0);
+
+        return complete;
     }
 }
